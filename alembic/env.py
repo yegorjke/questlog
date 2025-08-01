b76c21f9d59b8d420125d818de0bd887429e2e30
yegorjke <yegorjke@gmail.com>
2025-08-01 19:18:21 +0300

    Add alembic

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from questlog.core.config import settings  # <- добавь
+from questlog.core.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
