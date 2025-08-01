5a8e560fd30db88ff3615576c6ef802f75b12493
yegorjke <yegorjke@gmail.com>
2025-08-01 20:16:09 +0300

    Fix alembic and generate the initial migration

--- alembic/env.py
+++ alembic/env.py
@@ -7,28 +7,15 @@ from alembic import context
 
 from questlog.core.config import settings
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+from questlog.db.base import Base
+
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.database_url)
 
-
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
+target_metadata = Base.metadata
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- questlog/core/config.py
+++ questlog/core/config.py
@@ -1,9 +1,11 @@
-from pydantic import BaseSettings
+
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
 
     class Config:
-        env_file = ".env"
+        env_file = ".env.migrations"
 
 settings = Settings()
+
